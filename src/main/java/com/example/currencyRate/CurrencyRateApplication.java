506551953aae7f85170fae1403eb686f14307e1f
kuba <kubful21@gmail.com>
2025-07-01 09:46:16 +0200

    Inital commit

---
+++ src/main/java/com/example/currencyRate/CurrencyRateApplication.java
@@ -0,0 +1,15 @@
+package com.example.currencyRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CurrencyRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateApplication.class, args);
+	}
+
+}
